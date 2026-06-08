f8257dd42571485a84919d53b9681acd50be21c1
Diana Khatuaeva <dana.khatuaeva@gmail.com>
2026-06-08 19:23:15 +0300

    update

--- android-app/app/src/main/java/ru/mindflow/app/MainActivity.kt
+++ android-app/app/src/main/java/ru/mindflow/app/MainActivity.kt
@@ -4,44 +4,44 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.navigation.compose.rememberNavController
+import ru.mindflow.app.presentation.navigation.NavGraph
+import ru.mindflow.app.presentation.navigation.Screen
 import ru.mindflow.app.ui.theme.MindFlowTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var container: AppContainer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        container = AppContainer(applicationContext)
         enableEdgeToEdge()
         setContent {
             MindFlowTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                val navController = rememberNavController()
+                var startDestination by remember { mutableStateOf<String?>(null) }
+
+                LaunchedEffect(Unit) {
+                    startDestination = if (container.authRepository.isLoggedIn())
+                        Screen.Home.route
+                    else
+                        Screen.Login.route
+                }
+
+                startDestination?.let { start ->
+                    NavGraph(
+                        navController = navController,
+                        container = container,
+                        startDestination = start
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MindFlowTheme {
-        Greeting("Android")
     }
 }
