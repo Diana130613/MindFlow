749e5eb4ad450eb90d537207826cc5ed48853e85
Diana Khatuaeva <dana.khatuaeva@gmail.com>
2026-06-09 13:15:37 +0300

    стили

--- android-app/app/src/main/java/ru/mindflow/app/ui/theme/Color.kt
+++ android-app/app/src/main/java/ru/mindflow/app/ui/theme/Color.kt
@@ -2,10 +2,21 @@ package ru.mindflow.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Navy blue palette (matches design mockup)
+val NavyDeep    = Color(0xFF0F1A4A)
+val NavyBase    = Color(0xFF152158)
+val NavyCard    = Color(0xFF1B2A68)
+val NavyBright  = Color(0xFF243A8C)
+val NavyAccent  = Color(0xFF3B5ABD)
+val SkyBlue     = Color(0xFF5B7BDA)
+val NavyGhost   = Color(0xFF8BA3E0)
+val NavyWhite   = Color(0xFFFFFFFF)
+val ErrorRed    = Color(0xFFCF6679)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Gradient colors for recommendation cards
+val GradPurple1 = Color(0xFF7C3AED)
+val GradPink1   = Color(0xFFEC4899)
+val GradIndigo1 = Color(0xFF4338CA)
+val GradViolet1 = Color(0xFF9333EA)
+val GradBlue1   = Color(0xFF1D4ED8)
+val GradCyan1   = Color(0xFF0891B2)
